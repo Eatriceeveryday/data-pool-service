3fceb2b797aaaeb2b9f476e0e695829abaa43434
Eatriceeveryday <mufr31236@gmail.com>
2025-08-22 14:39:01 +0700

    chore: create SensorReport struct

---
+++ internal/entities/sensorReport.go
@@ -0,0 +1,9 @@
+package entities
+
+import "time"
+
+type SensorReport struct {
+	ReportID    int       `gorm:"primaryKey,autoIncrement"`
+	SensorValue float32   `gorm:"not null"`
+	Timestamp   time.Time `gorm:"not null"`
+}
