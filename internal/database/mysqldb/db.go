06207a908033b9b1a03bccdec45462ad45c8d583
Eatriceeveryday <mufr31236@gmail.com>
2025-08-22 15:33:57 +0700

    chore: add mysql database connection

---
+++ internal/database/mysqldb/db.go
@@ -0,0 +1,26 @@
+package mysqldb
+
+import (
+	"fmt"
+
+	"github.com/Eatriceeveryday/data-pool-service/internal/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func ConnectToDatabase(cfg *config.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		cfg.DBUsername, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
