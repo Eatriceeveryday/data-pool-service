a9643704a1c68290b1abdf4293a21d1c41ca9230
Eatriceeveryday <mufr31236@gmail.com>
2025-08-23 07:11:35 +0700

    chore: fix email not unique tag

--- internal/entities/user.go
+++ internal/entities/user.go
@@ -3,7 +3,7 @@ package entities
 type User struct {
 	UserID   uint   `gorm:"primaryKey;autoIncrement;index;unique"`
 	FullName string `gorm:"not null"`
-	Email    string `gorm:"not null"`
+	Email    string `gorm:"not null;unique"`
 	Password string `gorm:"not null"`
 
 	//Relation
