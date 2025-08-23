5d0b9f416092352447648178b8daaff47653e211
Eatriceeveryday <mufr31236@gmail.com>
2025-08-23 09:14:38 +0700

    feat: add getUser func to userService and fix duplicate email key error

--- internal/service/userService.go
+++ internal/service/userService.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/Eatriceeveryday/data-pool-service/internal/entities"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
@@ -25,10 +27,24 @@ func (s *UserService) CreateUser(user entities.User) (uint, error) {
 	result := s.db.Create(&user)
 
 	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+			return 0, gorm.ErrDuplicatedKey
+		}
 		return 0, err
 	}
 
 	return user.UserID, nil
+}
+
+func (s *UserService) GetUser(email string) (entities.User, error) {
+	var user entities.User
+
+	err := s.db.Where("email = ?", email).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return entities.User{}, errors.New("Record Not Found")
+	}
+
+	return user, nil
 }
 
 func (s *UserService) hashPassword(password string) (string, error) {
