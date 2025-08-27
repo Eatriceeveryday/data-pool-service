867cc469794c1e9e219cda8edb988837049fd508
Eatriceeveryday <mufr31236@gmail.com>
2025-08-27 11:04:05 +0700

    chore: fix tag

--- internal/entities/sensor.go
+++ internal/entities/sensor.go
@@ -1,12 +1,13 @@
 package entities
 
 type Sensor struct {
-	SensorID   uint   `gorm:"primaryKey;autoIncrement;index"`
-	ID1        string `gorm:"type:Varchar(1);not null;index"`
-	ID2        int    `gorm:"not null;index"`
-	SensorType string `gorm:"not null"`
+	SensorID   uint   `gorm:"primaryKey;autoIncrement;index" json:"sensorId"`
+	ID1        string `gorm:"type:Varchar(1);not null;index" json:"id1"`
+	ID2        int    `gorm:"not null;index" json:"id2"`
+	SensorType string `gorm:"not null" json:"sensorType"`
+	SensorKey  string `gorm:"not null" json:"sensorKey"`
 
 	//Relation
-	UserID  uint           `gorm:"not null;index"`
+	UserID  uint           `gorm:"not null;index"  json:"userId"`
 	Reports []SensorReport `gorm:"foreignKey:SensorID;references:SensorID"`
 }
