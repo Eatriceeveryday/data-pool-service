1d65f86a8cd7428dab5d4b9d237eda102cf6b13a
Eatriceeveryday <mufr31236@gmail.com>
2025-08-27 21:42:51 +0700

    chore: add entity delete report by id and duration

---
+++ http/v1/sensor/deleteSensorReportByIDandDurationRequest.go
@@ -0,0 +1,10 @@
+package sensor
+
+import "time"
+
+type DeleteSensorReportByIDandDurationRequest struct {
+	ID1   string    `json:"id1" validate:"required"`
+	ID2   int       `json:"id2" validate:"required"`
+	Start time.Time `json:"start" validate:"required"`
+	End   time.Time `json:"end" validate:"required"`
+}
