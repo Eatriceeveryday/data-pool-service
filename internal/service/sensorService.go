b66eb817909aa5d2f6cedeabf53fe15b15055e3b
Eatriceeveryday <mufr31236@gmail.com>
2025-08-27 16:06:10 +0700

    feat : add sensor service

---
+++ internal/service/sensorService.go
@@ -0,0 +1,149 @@
+package service
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/Eatriceeveryday/data-pool-service/internal/entities"
+	"github.com/golang-jwt/jwt/v5"
+	"gorm.io/gorm"
+)
+
+type SensorService struct {
+	mdb *gorm.DB //MysqlDB main db
+	pdb *gorm.DB //PostgreDB for emqx auth
+}
+
+func NewSensorService(mdb *gorm.DB, pdb *gorm.DB) *SensorService {
+	return &SensorService{mdb: mdb, pdb: pdb}
+}
+
+func (s *SensorService) CreateSensor(sensor entities.Sensor) (entities.Sensor, error) {
+	newSensor := entities.Sensor{
+		UserID:     sensor.UserID,
+		ID1:        sensor.ID1,
+		ID2:        sensor.ID2,
+		SensorType: sensor.SensorType,
+	}
+
+	if err := s.mdb.Create(&newSensor).Error; err != nil {
+		return entities.Sensor{}, err
+	}
+
+	key, err := createApiKey(newSensor.SensorID)
+	if err != nil {
+		return entities.Sensor{}, fmt.Errorf("failed to generate sensor key: %w", err)
+	}
+
+	if err := s.pdb.Create(&entities.MqttSensorKey{
+		SensorKey: key,
+	}).Error; err != nil {
+		return entities.Sensor{}, err
+	}
+	newSensor.SensorKey = key
+	fmt.Println("New Sensor : ", newSensor)
+	return newSensor, nil
+}
+
+func (s *SensorService) CreateReport(msg entities.Message) error {
+	parsedTime, err := time.Parse(time.RFC3339, msg.Timestamp)
+	if err != nil {
+		return fmt.Errorf("invalid timestamp: %w", err)
+	}
+
+	token, _, err := new(jwt.Parser).ParseUnverified(msg.Key, jwt.MapClaims{})
+	if err != nil {
+		return fmt.Errorf("invalid jwt: %w", err)
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return fmt.Errorf("invalid claims format")
+	}
+
+	sensorId := uint(claims["id"].(float64))
+
+	newReport := entities.SensorReport{
+		SensorValue: float32(msg.Value),
+		Timestamp:   parsedTime,
+		SensorID:    sensorId,
+	}
+
+	if err := s.mdb.Create(&newReport).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *SensorService) GetSensor(id1 string, id2 int, userId uint) (uint, error) {
+	var sensor entities.Sensor
+	err := s.mdb.Where("id1 = ? AND id2 = ? AND user_id = ?", id1, id2, userId).First(&sensor).Error
+	if err != nil {
+		return 0, err
+	}
+
+	return sensor.SensorID, nil
+}
+
+func (s *SensorService) GetReportWithDuration(sensorId uint, start time.Time, end time.Time, page int) ([]entities.SensorReport, int64, error) {
+	var reports []entities.SensorReport
+	var total int64
+
+	err := s.mdb.Model(&entities.SensorReport{}).
+		Where("sensor_id = ? AND timestamp BETWEEN ? AND ?", sensorId, start, end).
+		Count(&total).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	offset := (page - 1) * 25
+	err = s.mdb.Where("sensor_id = ? AND timestamp BETWEEN ? AND ?", sensorId, start, end).
+		Order("timestamp DESC").
+		Limit(10).
+		Offset(offset).
+		Find(&reports).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return reports, total, nil
+}
+
+func (s *SensorService) GetReportWithId(sensorId uint, page int) ([]entities.SensorReport, int64, error) {
+	var reports []entities.SensorReport
+	var total int64
+
+	err := s.mdb.Model(&entities.SensorReport{}).
+		Where("sensor_id = ? ", sensorId).
+		Count(&total).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	offset := (page - 1) * 25
+	err = s.mdb.Where("sensor_id = ? ", sensorId).
+		Order("timestamp DESC").
+		Limit(10).
+		Offset(offset).
+		Find(&reports).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return reports, total, nil
+}
+
+func createApiKey(sensorId uint) (string, error) {
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id": sensorId,
+	})
+
+	token, err := claims.SignedString([]byte(os.Getenv("ACCESS_KEY")))
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
