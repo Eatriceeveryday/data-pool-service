4fe8c7c7b3cc989df55eeb1c72542ffbc31a91be
Eatriceeveryday <mufr31236@gmail.com>
2025-08-27 11:12:06 +0700

    chore: add postgresql and emqx config load

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,13 +9,24 @@ import (
 var cfg *Config
 
 type Config struct {
-	DBUsername string
-	DBPassword string
-	DBHost     string
-	DBPort     string
-	DBName     string
+	MYSQLDBUsername string
+	MYSQLDBPassword string
+	MYSQLDBHost     string
+	MYSQLDBPort     string
+	MYSQLDBName     string
+
+	PSQLDBUsername string
+	PSQLDBPassword string
+	PSQLDBHost     string
+	PSQLDBPort     string
+	PSQLDBName     string
 
 	AccessKey string
+
+	EMQX_host     string
+	EMQX_port     string
+	EMQX_username string
+	EMQX_password string
 }
 
 func LoadConfig() (*Config, error) {
@@ -25,12 +36,24 @@ func LoadConfig() (*Config, error) {
 	}
 
 	cfg := &Config{
-		DBUsername: os.Getenv("DB_USERNAME"),
-		DBPassword: os.Getenv("DB_PASSWORD"),
-		DBHost:     os.Getenv("DB_HOST"),
-		DBPort:     os.Getenv("DB_PORT"),
-		DBName:     os.Getenv("DB_NAME"),
-		AccessKey:  os.Getenv("ACCESS_KEY"),
+		MYSQLDBUsername: os.Getenv("MYSQL_DB_USERNAME"),
+		MYSQLDBPassword: os.Getenv("MYSQL_DB_PASSWORD"),
+		MYSQLDBHost:     os.Getenv("MYSQL_DB_HOST"),
+		MYSQLDBPort:     os.Getenv("MYSQL_DB_PORT"),
+		MYSQLDBName:     os.Getenv("MYSQL_DB_NAME"),
+
+		PSQLDBUsername: os.Getenv("PSQL_DB_USERNAME"),
+		PSQLDBPassword: os.Getenv("PSQL_DB_PASSWORD"),
+		PSQLDBHost:     os.Getenv("PSQL_DB_HOST"),
+		PSQLDBPort:     os.Getenv("PSQL_DB_PORT"),
+		PSQLDBName:     os.Getenv("PSQL_DB_NAME"),
+
+		AccessKey: os.Getenv("ACCESS_KEY"),
+
+		EMQX_host:     os.Getenv("EMQX_HOST"),
+		EMQX_port:     os.Getenv("EMQX_PORT"),
+		EMQX_username: os.Getenv("EMQX_USERNAME"),
+		EMQX_password: os.Getenv("EMQX_PASSWORD"),
 	}
 	return cfg, nil
 }
